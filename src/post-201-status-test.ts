8dee8f39ce87954a5d71999479924b2b823e75e8
datguychen <adam.tomasz.swiderski@gmail.com>
2023-10-14 17:16:24 +0200

    New test + TS updated

---
+++ src/post-201-status-test.ts
@@ -0,0 +1,33 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export let options = {
+  vus: 1,
+  duration: '10s',
+};
+
+export default function () {
+  let username = Math.random().toString(36).substring(7);
+  let email = `${username}@example.com`;
+  let password = Math.random().toString(36).substring(7);
+
+  let payload = JSON.stringify({
+    username: username,
+    email: email,
+    password: password
+  });
+
+  let params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  let res = http.post('https://test-api.k6.io/user/register/', payload, params);
+  console.log(res.body);
+
+  check(res, {
+    'status is 201': (r) => r.status === 201,
+  });
+  sleep(1);
+}
