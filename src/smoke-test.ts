f32ae6d65ff14f6f2b3d4ac73ac4a4bab9152867
datguychen <adam.tomasz.swiderski@gmail.com>
2023-11-12 18:40:10 +0100

    smoke test v2

--- src/smoke-test.ts
+++ src/smoke-test.ts
@@ -4,17 +4,17 @@ import http from 'k6/http';
 
 export let options:Options = {
   vus: 1,
-  duration: '30s'
+  duration: '10s'
 };
 
 export default () => {
-  const res = http.get('https://test-api.k6.io');
+  const res = http.get('https://test-api.k6.io/');
   check(res, {
     'status is 200': () => res.status === 200,
   });
   sleep(1); //1 call per second
-  http.get('https://test-api.k6.io/contacts.php');
+  http.get('https://test-api.k6.io/public/crocodiles/1/'); //check first endpoint
   sleep(2);
-  http.get('https://test-api.k6.io/news.php');
+  http.get('https://test-api.k6.io/public/crocodiles/'); //check second endpoint
   sleep(2);
 };
