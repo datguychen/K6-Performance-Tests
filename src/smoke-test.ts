5de2c577f77d1b30d89905a8cd533b90ae4ace28
datguychen <adam.tomasz.swiderski@gmail.com>
2023-11-12 18:44:34 +0100

    smoke test v3

--- src/smoke-test.ts
+++ src/smoke-test.ts
@@ -2,15 +2,17 @@ import { sleep, check } from 'k6';
 import { Options } from 'k6/options';
 import http from 'k6/http';
 
+//We need only 1 VU for this test, because smoke test only checks if the API is up and running properly
+
 export let options:Options = {
   vus: 1,
   duration: '10s'
 };
 
 export default () => {
-  const res = http.get('https://test-api.k6.io/');
-  check(res, {
-    'status is 200': () => res.status === 200,
+  const response = http.get('https://test-api.k6.io/');
+  check(response, {
+    'status is 200': () => response.status === 200,
   });
   sleep(1); //1 call per second
   http.get('https://test-api.k6.io/public/crocodiles/1/'); //check first endpoint
